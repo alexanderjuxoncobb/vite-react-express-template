d952357bdb0f1f1918711666396f7975e514b8a0
alexanderjuxoncobb <alexanderjuxoncobb@gmail.com>
2025-04-03 11:16:16 +0100

    SPA with React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
-function App() {
+// Page components
+function Home() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
@@ -19,10 +21,66 @@ function App() {
   }, []);
 
   return (
-    <>
-      <h1>Vite + React + Express</h1>
+    <div>
+      <h1>Home Page</h1>
       <p>API Message: {message}</p>
-    </>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page content.</p>
+    </div>
+  );
+}
+
+function Users() {
+  return (
+    <div>
+      <h1>Users Page</h1>
+      <p>This is where user information would appear.</p>
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for doesn't exist.</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
